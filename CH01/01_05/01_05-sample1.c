08474cd0a708631772497abf5fed4014fa328671
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-05-05 04:14:29 +0000

    Practice rewrite the code.

--- CH01/01_05/01_05-sample1.c
+++ CH01/01_05/01_05-sample1.c
@@ -1,18 +1,35 @@
-/* input and loop demo */
+// /* input and loop demo */
+// #include <stdio.h>
+
+// int main()
+// {
+// 	int a,b;
+
+// 	printf("Type a positive value: ");
+// 	scanf("%d",&b);
+// 	for(a=0;a<b;a++)
+// 	{
+// 		printf("I must do this %d times\n",b);
+// 		if(a==9)
+// 			break;
+// 	}
+
+// 	return 0;
+// }
 #include <stdio.h>
 
 int main()
 {
-	int a,b;
-
-	printf("Type a positive value: ");
-	scanf("%d",&b);
-	for(a=0;a<b;a++)
+	int a, b;
+	printf("Type a positive number: ");
+	scanf("%d", &b);
+	for (a = 0; a < b; ++a)
 	{
-		printf("I must do this %d times\n",b);
-		if(a==9)
+		printf("I love you %d times than you love me.\n", b);
+		if (a == 9)
+		{
 			break;
+		}
 	}
-	
 	return 0;
 }

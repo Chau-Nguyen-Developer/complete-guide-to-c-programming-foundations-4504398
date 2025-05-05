36737fd112d373e5e8de1c0d6e173ee073449762
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-05-05 04:00:19 +0000

    Added comment for putchar() function

--- CH01/01_04/01_04-hello1.c
+++ CH01/01_04/01_04-hello1.c
@@ -2,15 +2,16 @@
 
 int main()
 {
+	// Declares and initializes a character array named "string"
 	char string[] = "Greetings, human!\n";
 	int index;
 
 	index = 0;
-	while( string[index] != '\0' )
+	// loop through each character in the array and print them one by one
 	{
-		putchar( string[index] );
+		putchar(string[index]);
 		index++;
 	}
 
-	return(0);
+	return (0);
 }

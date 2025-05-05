4b6f2342321bacbac42e5845656d6b5712982856
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-05-05 03:29:09 +0000

    Initial commit

--- CH01/01_01/01_01-first1.c
+++ CH01/01_01/01_01-first1.c
@@ -1,2 +1,10 @@
+#include <stdio.h>
+//The "include" is pre-processor directive.
 
+int main()
+{
+  puts("Good morning!");
+
+  return 0;
+}
 
--- CH01/01_02/01_02-challenge1.c
+++ CH01/01_02/01_02-challenge1.c
@@ -1,2 +1,8 @@
+// PROGRAMMER: NGOC CHAU NGUYEN
+#include <stdio.h>
 
-
+int main()
+{
+  puts("I am a C programmer.");
+  return 0;
+}

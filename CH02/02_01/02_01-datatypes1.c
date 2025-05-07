3922dfb223ab29e3ae139da950dffe7964a45488
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-05-07 19:47:29 +0000

    Looked more into file-gets-string fgets() functions. Added in notes.

--- CH02/02_01/02_01-datatypes1.c
+++ CH02/02_01/02_01-datatypes1.c
@@ -9,18 +9,34 @@ int main()
 
 	/* input */
 	printf("Type a character: ");
-	scanf("%c",&c);
+	// Scan in address.
+	// The scanf function reads in the input.
+	// First agrument: the type of data. Second argument: the variable's address.
+	scanf("%c", &c);
 	printf("Type an integer: ");
-	scanf("%d",&i);
+	scanf("%d", &i);
 	printf("Type a small value: ");
-	scanf("%f",&f);
+	scanf("%f", &f);
 	printf("Type a huge value: ");
-	scanf("%lg",&d);
+	scanf("%lg", &d);
 
 	/* output */
-	printf("\nCharacter: %c\n",c);
-	printf("Integer: %d\n",i);
-	printf("Single-precision: %f\n",f);
-	printf("Double-precision: %lg\n",d);
+	// Don't need to pass in the address of data in the printf function.
+	printf("\nCharacter: %c\n", c);
+	printf("Integer: %d\n", i);
+	printf("Single-precision: %f\n", f);
+	printf("Double-precision: %lg\n", d);
 	return 0;
 }
+
+// Another data type: "void" data type
+/*Void for no data type or an undefined data type.
+This type comes into play for allocating memory
+and referencing functions that return no value or accept no argument.
+(used when allocating memory and used for function definitions)
+
+
+Programming language C has no string or text data type.
+An array of char variables is used for strings.
+
+*/
--- CH02/02_01/02_01-string1.c
+++ CH02/02_01/02_01-string1.c
@@ -2,10 +2,25 @@
 
 int main()
 {
+	//Declares a character array (string) that
+	//can hold up to 31 characters and a null terminator (\0).
 	char buffer[32];
 
+	//Print out the prompt to the terminal, asking users for input.
 	printf("Type your name: ");
+
+	//char *fgets(char *restrict str, int count, FILE *restrict stream);
+	//Read at most 31 characters from the given file stream (stdin), 
+	//and stores them in the character array pointed to by "buffer". 
 	fgets(buffer,32,stdin);
+	
 	printf("Hello, %s",buffer);
 	return 0;
 }
+
+/* Note:
+char* fgets( char* restrict str, int count, FILE* restrict stream );
+Reads at most (count - 1) characters from the given file stream and stores them in the character array pointed to by str.
+Parsing stops if a newline character is found (in which case str will contain that newline character) or if end-of-file occurs.
+If bytes are read and no errors occur, writes a null character at the position immediately after the last character written to str.
+*/

900a9c1a869e33277c0e8684477d225a95d519d5
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-05-11 08:15:39 +0000

    Added notes when not initialized variables and use them straight.

--- CH02/02_03/02_03-variable4.c
+++ CH02/02_03/02_03-variable4.c
@@ -2,9 +2,15 @@
 
 int main()
 {
+	//Float variable x is not initialized.
 	float x;
 
 	printf("The value of %f is unknown\n",x);
+
+	// Might encounter compiler warning.
+	//The value output represents whatever data exists in memory at that location. 
+	// It could be zero, and it also could be some other value. 
+	 
 
 	return(0);
 }

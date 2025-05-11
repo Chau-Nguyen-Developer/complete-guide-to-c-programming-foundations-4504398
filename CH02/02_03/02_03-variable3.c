ef5436cd71f0820ca8af35baa1ce607fb000adba
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-05-11 08:02:14 +0000

    Practiced rewrite code. Used my name as example.

--- CH02/02_03/02_03-variable3.c
+++ CH02/02_03/02_03-variable3.c
@@ -1,16 +1,31 @@
+// #include <stdio.h>
+
+// int main()
+// {
+// 	// These five variables are of the same type.
+// 	char a, b, c, d, e;
+
+// 	a = 'H';
+// 	b = 'e';
+// 	c = 'l';
+// 	d = 'o';
+// 	e = '\n';
+
+// 	// Use the char c twice to represent the duplicate L.
+// 	printf("%c%c%c%c%c%c", a, b, c, c, d, e);
+
+// 	return (0);
+// }
+
 #include <stdio.h>
 
 int main()
 {
-	char a,b,c,d,e;
-
-	a = 'H';
-	b = 'e';
-	c = 'l';
-	d = 'o';
-	e = '\n';
-
-	printf("%c%c%c%c%c%c",a,b,c,c,d,e);
-
-	return(0);
+	char a, b, c, d;
+	a = 'c';
+	b = 'h';
+	c = 'a';
+	d = 'u';
+	printf("%c%c%c%c", a, b, c, d);
+	return 0;
 }

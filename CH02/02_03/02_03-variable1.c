2d5f896dd14a3991e127dd8a94b735512537290b
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-05-11 07:50:31 +0000

    Accidently used %d (for int) for both variable c (float/single precision) and variable d (large value/ double precision).Undefined behavior. Got random number. Fixed the bug. Changed %d to %f. Got the expected results.

--- CH02/02_03/02_03-variable1.c
+++ CH02/02_03/02_03-variable1.c
@@ -18,8 +18,8 @@ int main()
 	printf("c is %f\n", c);
 	printf("d is %f\n", d);
 	printf("b is %d\n", b);
-	printf("c is %d\n", c);
-	printf("d is %d\n", d);
+	printf("c is %f\n", c);
+	printf("d is %f\n", d);
 
 	return (0);
 }

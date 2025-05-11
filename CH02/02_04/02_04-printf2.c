2dec1f49b6abc77d6f6aa76375e3559346790d21
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-05-11 08:23:56 +0000

    Can treat int as ASCII code. Will interpret that into character.

--- CH02/02_04/02_04-printf2.c
+++ CH02/02_04/02_04-printf2.c
@@ -4,6 +4,15 @@ int main()
 {
 	int a = 65;
 
-	printf("The value of 'a' is %d\n",a);
+	printf("1. The value of 'a' is %c.\n", a);
+	// Will print out A. Treat number 65 as ASCII code.
+	// Number 65 --> Upper case A.
+
+	printf("2. The value of 'a' is %d.\n", a);
+	// Treat number 65 as integer 65.
+
+	printf("2. The value of 'a' is %f.\n", a);
+	//incorrect data type. Integer can be cast/ interpreted as char, but not for double. 
 	return 0;
 }
+
